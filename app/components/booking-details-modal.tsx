1d874cf886f96030f0c175835563350278c8a6cb
Afsal Azizi <contact.afsalaziz@gmail.com>
2025-10-22 15:12:24 +0400

    fix: code error

--- app/components/booking-details-modal.tsx
+++ app/components/booking-details-modal.tsx
@@ -248,10 +248,8 @@ export function BookingDetailsModal({
                             "px-2 inline-flex text-xs leading-5 font-semibold rounded-full",
                             service.status === "completed" &&
                               "bg-green-100 text-green-800",
-                            service.status === "in-progress" &&
+                            service.status === "in_progress" &&
                               "bg-amber-100 text-amber-800",
-                            service.status === "confirmed" &&
-                              "bg-blue-100 text-blue-800",
                             service.status === "pending" &&
                               "bg-yellow-100 text-yellow-800",
                             service.status === "cancelled" &&
@@ -402,7 +400,7 @@ export function BookingDetailsModal({
                           "px-2 inline-flex text-xs leading-5 font-semibold rounded-full",
                           booking.status === "completed" &&
                             "bg-green-100 text-green-800",
-                          booking.status === "in-progress" &&
+                          booking.status === "in_progress" &&
                             "bg-amber-100 text-amber-800",
                           booking.status === "confirmed" &&
                             "bg-blue-100 text-blue-800",
